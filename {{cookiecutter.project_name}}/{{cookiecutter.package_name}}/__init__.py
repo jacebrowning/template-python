762a7a1710c29e4984296fa37305da3258f6fb62
Josh Friend <josh@fueledbycaffeine.com>
2014-11-06 10:03:37 -0500

    missed a few template things on cookiecutter conversion

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-"""Package for Foobar."""
+"""Package for {{cookiecutter.project_name}}."""
 
-__project__ = 'Foobar'
+__project__ = '{{cookiecutter.project_name}}'
 __version__ = '0.0.0'
 
 VERSION = __project__ + '-' + __version__
