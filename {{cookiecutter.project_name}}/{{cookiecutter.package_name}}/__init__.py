fc2050cd48cfcab1f3f8565a2e3d93c81d4106e7
Jace Browning <jacebrowning@gmail.com>
2015-01-18 12:03:46 -0500

    Add explicit version check

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -4,3 +4,9 @@ __project__ = '{{cookiecutter.project_name}}'
 __version__ = '0.0.0'
 
 VERSION = __project__ + '-' + __version__
+
+PYTHON_VERSION = {{cookiecutter.python_major_version}}, {{cookiecutter.python_minor_version}}
+
+import sys
+if not sys.version_info >= PYTHON_VERSION:  # pragma: no cover (manual test)
+    exit("Python {}.{}+ is required.".format(*PYTHON_VERSION))
