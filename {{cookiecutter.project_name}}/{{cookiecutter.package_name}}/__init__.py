1a1b9bdf0e4ea252bd9148cd755e4f3cbe93095c
Jace Browning <jacebrowning@gmail.com>
2015-02-06 23:03:07 -0500

    Fix PEP8 E402

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,5 +1,7 @@
 """Package for {{cookiecutter.project_name}}."""
 
+import sys
+
 __project__ = '{{cookiecutter.project_name}}'
 __version__ = '0.0.0'
 
@@ -7,6 +9,5 @@ VERSION = __project__ + '-' + __version__
 
 PYTHON_VERSION = {{cookiecutter.python_major_version}}, {{cookiecutter.python_minor_version}}
 
-import sys
 if not sys.version_info >= PYTHON_VERSION:  # pragma: no cover (manual test)
     exit("Python {}.{}+ is required.".format(*PYTHON_VERSION))
