80928e1e529588ffa2744696b9315a73a58c0526
Jace Browning <jacebrowning@gmail.com>
2015-11-30 17:56:20 -0500

    Invert version comparison logic

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -9,5 +9,5 @@ VERSION = "{0} v{1}".format(__project__, __version__)
 
 PYTHON_VERSION = {{cookiecutter.python_major_version}}, {{cookiecutter.python_minor_version}}
 
-if not sys.version_info >= PYTHON_VERSION:  # pragma: no cover (manual test)
+if sys.version_info < PYTHON_VERSION:  # pragma: no cover (manual test)
     exit("Python {}.{}+ is required.".format(*PYTHON_VERSION))
