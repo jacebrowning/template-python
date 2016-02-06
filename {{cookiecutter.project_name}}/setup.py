2d3350c8b5524af80864ffa1dfa8493ff95cec6f
Jace Browning <jacebrowning@gmail.com>
2016-02-06 01:57:38 -0500

    Generated CHANGES.rst from CHANGES.md
    Closes #105

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -6,13 +6,13 @@ import setuptools
 
 from {{cookiecutter.package_name}} import __project__, __version__
 
-import os
-if os.path.exists('README.rst'):
-    README = open('README.rst').read()
+try:
+    README = open("README.rst").read()
+    CHANGES = open("CHANGES.rst").read()
+except IOError:
+    DESCRIPTION = "<placeholder>"
 else:
-    README = ""  # a placeholder until README is generated on release
-CHANGES = open('CHANGES.md').read()
-
+    DESCRIPTION = README + '\n' + CHANGES
 
 setuptools.setup(
     name=__project__,
@@ -27,7 +27,7 @@ setuptools.setup(
 
     entry_points={'console_scripts': []},
 
-    long_description=(README + '\n' + CHANGES),
+    long_description=(DESCRIPTION),
     license='MIT',
     classifiers=[
         # TODO: update this list to match your application: https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -43,5 +43,5 @@ setuptools.setup(
         'Programming Language :: Python :: 3.5',
     ],
 
-    install_requires=open('requirements.txt').readlines(),
+    install_requires=open("requirements.txt").readlines(),
 )
