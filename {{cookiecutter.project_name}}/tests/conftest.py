7bf94a972628a708129645d505688a9b28858144
Jace Browning <jacebrowning@gmail.com>
2016-05-25 14:07:26 -0400

    Rename the unit tests package to 'tests'
    And now ‘make test’ will run all the tests.

--- {{cookiecutter.project_name}}/tests/conftest.py
+++ {{cookiecutter.project_name}}/tests/conftest.py
@@ -1,3 +1,3 @@
 """Integration tests configuration file."""
 
-from {{cookiecutter.package_name}}.test.conftest import pytest_configure  # pylint: disable=unused-import
+from {{cookiecutter.package_name}}.tests.conftest import pytest_configure  # pylint: disable=unused-import
