604ed9e4260838584c80440cb14f66a44d812e2f
Jace Browning <jacebrowning@gmail.com>
2016-09-16 16:03:01 -0400

    Only rebuild dist files when needed

--- {{cookiecutter.project_name}}/scent.py
+++ {{cookiecutter.project_name}}/scent.py
@@ -35,8 +35,8 @@ def python(*_):
     """Run targets for Python."""
 
     for count, (command, title, retry) in enumerate((
-        (('make', 'test-unit'), "Unit Tests", True),
-        (('make', 'test-int'), "Integration Tests", False),
+        (('make', 'test-unit', 'CI=true'), "Unit Tests", True),
+        (('make', 'test-int', 'CI=true'), "Integration Tests", False),
         (('make', 'test-all'), "Combined Tests", False),
         (('make', 'check'), "Static Analysis", True),
         (('make', 'doc'), None, True),
