6f744ec7bb911f6fa7668470093968f63c7e2008
Jace Browning <jacebrowning@gmail.com>
2016-09-16 21:13:42 -0400

    Update sample Tkinter code to use * import
    This lines up with the documentation better.

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/sample.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/sample.py
@@ -1,11 +1,11 @@
 """A sample module."""
 
 {%- if cookiecutter.python_major_version == "3" %}
-import tkinter as tk
-from tkinter import ttk
+from tkinter import *  # pylint: disable=wildcard-import,unused-wildcard-import
+from tkinter.ttk import *  # pylint: disable=wildcard-import,unused-wildcard-import
 {%- else %}
-import Tkinter as tk
-import ttk
+from Tkinter import *  # pylint: disable=wildcard-import,unused-wildcard-import
+from ttk import *  # pylint: disable=wildcard-import,unused-wildcard-import
 {%- endif %}
 import logging
 
@@ -20,31 +20,31 @@ class Application(object):
         log.info("Starting the application...")
 
         # Configure the root window
-        self.root = tk.Tk()
+        self.root = Tk()
         self.root.title("Feet to Meters")
         self.root.minsize(400, 150)
 
         # Initialize elements
-        self.feet = tk.StringVar()
-        self.meters = tk.StringVar()
+        self.feet = StringVar()
+        self.meters = StringVar()
         frame = self.init(self.root)
-        frame.pack(fill=tk.BOTH, expand=1)
+        frame.pack(fill=BOTH, expand=1)
 
         # Start the event loop
         self.root.mainloop()
 
     def init(self, root):
         padded = {'padding': 5}
-        sticky = {'sticky': tk.NSEW}
+        sticky = {'sticky': NSEW}
 
         # Configure grid
-        frame = ttk.Frame(root, **padded)
+        frame = Frame(root, **padded)
         frame.rowconfigure(0, weight=1)
         frame.rowconfigure(1, weight=1)
         frame.columnconfigure(0, weight=1)
 
         def frame_inputs(root):
-            frame = ttk.Frame(root, **padded)
+            frame = Frame(root, **padded)
 
             # Configure grid
             frame.rowconfigure(0, weight=1)
@@ -54,36 +54,36 @@ class Application(object):
             frame.columnconfigure(2, weight=1)
 
             # Place widgets
-            entry = ttk.Entry(frame, width=7, textvariable=self.feet)
+            entry = Entry(frame, width=7, textvariable=self.feet)
             entry.grid(column=1, row=0)
             entry.focus()
-            label = ttk.Label(frame, text="feet")
+            label = Label(frame, text="feet")
             label.grid(column=2, row=0)
-            label = ttk.Label(frame, text="is equivalent to")
+            label = Label(frame, text="is equivalent to")
             label.grid(column=0, row=1)
-            label = ttk.Label(frame, text="meters")
+            label = Label(frame, text="meters")
             label.grid(column=2, row=1)
-            label = ttk.Label(frame, textvariable=self.meters)
+            label = Label(frame, textvariable=self.meters)
             label.grid(column=1, row=1)
 
             return frame
 
         def frame_commands(root):
-            frame = ttk.Frame(root, **padded)
+            frame = Frame(root, **padded)
 
             # Configure grid
             frame.rowconfigure(0, weight=1)
             frame.columnconfigure(0, weight=1)
 
             # Place widgets
-            button = ttk.Button(frame, text="Calculate", command=self.calculate)
+            button = Button(frame, text="Calculate", command=self.calculate)
             button.grid(column=0, row=0)
             self.root.bind('<Return>', self.calculate)
 
             return frame
 
         def separator(root):
-            return ttk.Separator(root)
+            return Separator(root)
 
         # Place widgets
         frame_inputs(frame).grid(row=0, **sticky)
