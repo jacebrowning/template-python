537c22482a00703b5bd4391f25c8fb23d43ee692
Jace Browning <jacebrowning@gmail.com>
2016-09-23 21:27:24 -0400

    Add logging

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/tests/conftest.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/tests/conftest.py
@@ -1,8 +1,12 @@
 """Unit tests configuration file."""
 
+import logging
+
 
 def pytest_configure(config):
     """Disable verbose output when running tests."""
+    logging.basicConfig(level=logging.DEBUG)
+
     terminal = config.pluginmanager.getplugin('terminal')
     base = terminal.TerminalReporter
 
