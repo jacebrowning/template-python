6ae5c385235b88f2f8eeceabb7b517cd91c7e4b0
Jace Browning <jacebrowning@gmail.com>
2017-01-07 00:54:28 -0500

    Handle package variables with spaces

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -15,7 +15,7 @@ MINIMUM_PYTHON_VERSION = {{cookiecutter.python_major_version}}, {{cookiecutter.p
 def check_python_version():
     """Exit when the Python version is too low."""
     if sys.version_info < MINIMUM_PYTHON_VERSION:
-        sys.exit("Python {}.{}+ is required.".format(*MINIMUM_PYTHON_VERSION))
+        sys.exit("Python {0}.{1}+ is required.".format(*MINIMUM_PYTHON_VERSION))
 
 
 def read_package_variable(key):
@@ -23,10 +23,10 @@ def read_package_variable(key):
     module_path = os.path.join(PACKAGE_NAME, '__init__.py')
     with open(module_path) as module:
         for line in module:
-            parts = line.strip().split(' ')
-            if parts and parts[0] == key:
+            parts = line.strip().split(' ', 2)
+            if parts[:-1] == [key, '=']:
                 return parts[-1].strip("'")
-    assert 0, "'{0}' not found in '{1}'".format(key, module_path)
+    sys.exit("'{0}' not found in '{1}'".format(key, module_path))
 
 
 def read_descriptions():
