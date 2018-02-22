55f2f767d3a6379c5af3d3ea7eab5ba9df17f7d6
Jace Browning <jacebrowning@gmail.com>
2018-02-21 22:13:13 -0500

    Update pytest-expecter

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/tests/test_utils.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/tests/test_utils.py
@@ -18,16 +18,14 @@ class Test{{cookiecutter.package_name | capitalize}}(unittest.TestCase):
 """Sample unit test module using pytest-describe and expecter."""
 # pylint: disable=redefined-outer-name,unused-variable,expression-not-assigned,singleton-comparison
 
-from expecter import expect
-
 from {{cookiecutter.package_name}} import utils
 
 
 def describe_feet_to_meters():
 
-    def when_integer():
+    def when_integer(expect):
         expect(utils.feet_to_meters(42)) == 12.80165
 
-    def when_string():
+    def when_string(expect):
         expect(utils.feet_to_meters("hello")) == None
 {% endif -%}
