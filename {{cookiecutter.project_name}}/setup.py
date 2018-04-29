a3067ac9d594dff1520305e332b3f0385ca9dd75
Jace Browning <jacebrowning@gmail.com>
2018-04-28 21:46:44 -0400

    Upload Markdown to the new PyPI

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -29,13 +29,9 @@ def read_package_variable(key, filename='__init__.py'):
 
 def build_description():
     """Build a description for the project from documentation files."""
-    try:
-        readme = open("README.rst").read()
-        changelog = open("CHANGELOG.rst").read()
-    except IOError:
-        return "<placeholder>"
-    else:
-        return readme + '\n' + changelog
+    readme = open("README.md").read()
+    changelog = open("CHANGELOG.md").read()
+    return readme + '\n' + changelog
 
 
 check_python_version()
@@ -57,6 +53,7 @@ setuptools.setup(
     ]},
 
     long_description=build_description(),
+    long_description_content_type='text/markdown',
     license='MIT',
     classifiers=[
         # TODO: update this list to match your application: https://pypi.org/pypi?%3Aaction=list_classifiers
