97f2a708354e3354a93570a57845b0f7f170da04
Jace Browning <jacebrowning@gmail.com>
2018-08-20 17:29:38 -0400

    Switch to Poetry for dependency management

--- {{cookiecutter.project_name}}/scent.py
+++ {{cookiecutter.project_name}}/scent.py
@@ -16,7 +16,7 @@ else:
 watch_paths = ["{{cookiecutter.package_name}}", "tests"]
 
 
-class Options(object):
+class Options:
     group = int(time.time())  # unique per run
     show_coverage = False
     rerun_args = None
