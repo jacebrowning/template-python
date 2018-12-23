045ba3281e436c7512df8f72e17d8c5bc46d29fb
Jace Browning <jacebrowning@gmail.com>
2018-12-23 13:02:58 -0500

    Remove support for nose as a test runner

--- {{cookiecutter.project_name}}/tests/test_cli.py
+++ {{cookiecutter.project_name}}/tests/test_cli.py
@@ -1,24 +1,3 @@
-{%- if cookiecutter.test_runner == "nose" -%}
-"""Sample integration test module using nose."""
-# pylint: disable=no-self-use,missing-docstring
-
-import unittest
-
-from click.testing import CliRunner
-
-from {{cookiecutter.package_name}}.cli import main
-
-
-class Test{{cookiecutter.package_name | capitalize}}(unittest.TestCase):
-    """Sample integration test class."""
-
-    def test_conversion(self):
-        runner = CliRunner()
-        result = runner.invoke(main, ['42'])
-
-        self.assertEqual(result.exit_code, 0)
-        self.assertEqual(result.output, "12.80165\n")
-{% elif cookiecutter.test_runner == "pytest" -%}
 """Sample integration test module using pytest-describe and expecter."""
 # pylint: disable=redefined-outer-name,unused-variable,expression-not-assigned,singleton-comparison
 
@@ -50,4 +29,3 @@ def describe_cli():
 
             expect(result.exit_code) == 0
             expect(result.output) == ""
-{% endif -%}
