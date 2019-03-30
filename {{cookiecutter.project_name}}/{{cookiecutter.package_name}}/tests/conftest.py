cf70d043614b0442846a1211d5001705272b9191
Jace Browning <jacebrowning@gmail.com>
2019-03-30 14:46:41 -0400

    Update pytest

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/tests/conftest.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/tests/conftest.py
@@ -8,14 +8,4 @@ def pytest_configure(config):
     log.init(debug=True)
 
     terminal = config.pluginmanager.getplugin('terminal')
-
-    class QuietReporter(terminal.TerminalReporter):  # type: ignore
-        """Reporter that only shows dots when running tests."""
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.verbosity = 0
-            self.showlongtestinfo = False
-            self.showfspath = False
-
-    terminal.TerminalReporter = QuietReporter
+    terminal.TerminalReporter.showfspath = False
