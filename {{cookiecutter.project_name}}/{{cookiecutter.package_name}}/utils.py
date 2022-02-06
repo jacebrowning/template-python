b33500b18dcede84ee6ba8b6fe899ed227caa0df
Jace Browning <jacebrowning@gmail.com>
2022-02-06 16:15:07 -0500

    Update pylint

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/utils.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/utils.py
@@ -9,5 +9,6 @@ def feet_to_meters(feet):
         value = float(feet)
     except ValueError:
         log.error("Unable to convert to float: %s", feet)
+        return None
     else:
         return (0.3048 * value * 10000.0 + 0.5) / 10000.0
